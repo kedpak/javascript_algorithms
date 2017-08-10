d7b36261e1e0a1e21de63a9b7490196b9e3951f2
kedpak <kedpak@yahoo.com>
2017-08-10 08:51:21 +0000

    changed variable names for clarity

--- title_case.js
+++ title_case.js
@@ -7,20 +7,18 @@ function titleCase(str) {
 
     var arr = str.split(' ');
     var lower_str = [];
-    var new_str = [];
+    var cap_str = [];
     var final_arr = [];
 
     for (var i = 0; i < arr.length; i++) {
 	lower_str[i] = arr[i].substring(1).toLowerCase();
-	new_str[i] = arr[i].charAt(0).toUpperCase();
-	final_arr[i] = new_str[i] + lower_str[i];
+	cap_str[i] = arr[i].charAt(0).toUpperCase();
+	final_arr[i] = cap_str[i] + lower_str[i];
     }
 
     final_arr = final_arr.join(' ');
 
-    str = final_arr;
-
-    return str;
+    return final_arr;
 }
 
 titleCase("I'm a little tea pot");
